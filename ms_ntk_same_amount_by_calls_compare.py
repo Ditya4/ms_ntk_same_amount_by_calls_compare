3043797c574e9182d1bc405c804907c4b271d0a9
Ditya4 <72632156+Ditya4@users.noreply.github.com>
2020-11-10 15:45:20 +0200

    add ntk_calls class and create hash for it

--- ms_ntk_same_amount_by_calls_compare.py
+++ ms_ntk_same_amount_by_calls_compare.py
@@ -1,5 +1,5 @@
 from os import path
-from _ast import If
+
 
 class MsCalls:
     def __init__(self, index=None, switch_id=None, uni_a=None, uni_b=None,
@@ -47,6 +47,88 @@ class MsCalls:
                 str(self.duration))
         return hash
 
+
+class NtkCalls:
+    def __init__(self, index=None, recipient_id=None, number_a=None, number_b=None,
+                 date_start=None, duration=None, inc_tg=None,
+                 call_type=None, cdr_set_id=None):
+        self.index = index
+        self.recipient_id = recipient_id
+        self.number_a = number_a
+        self.number_b = number_b
+        self.date_start = date_start
+        self.duration = duration
+        self.inc_tg = inc_tg
+        self.call_type = call_type
+        self.cdr_set_id = cdr_set_id
+        # calculated fields
+        self.hash = self.hash_create()
+
+    def __str__(self):
+        '''
+        we return a string which almost looks like a list with str value
+        of every field in record
+        '''
+        list_of_values = [str((t)) for name, t in self.__dict__.items()
+                          if type(t).__name__ != "function" and
+                          not name.startswith("__")]
+        line_to_return = "[" + " , ".join(list_of_values) + "]"
+        return line_to_return
+    
+    def hash_create(self):
+        hash = ''
+        if len(self.number_b) == 10 and str(self.number_b)[0] == '0':
+            number_b = str(self.number_b[1:])
+        else:
+            number_b = str(self.number_b)
+        #=======================================================================
+        # if str(self.uni_a)[:3] == '380':
+        #     uni_a = str(self.uni_a)[3:]
+        # else:
+        #     uni_a = str(self.uni_a)
+        #=======================================================================
+        
+        hash = (str(self.recipient_id) + str(self.number_a) + number_b +
+                str(self.inc_tg) +
+                str(self.date_start).replace('.', '').replace(' ', '').replace(':', '') +
+                str(self.duration))
+        return hash
+
+def read_ntk_calls(folder, file_name):
+    ntk_calls_file_name = path.join(folder, file_name)
+    ntk_calls_file = open(ntk_calls_file_name)
+    ntk_calls_lines = ntk_calls_file.readlines()
+    size_of_ntk_calls_list = len(ntk_calls_lines)
+    for index in range(size_of_ntk_calls_list):
+        ntk_calls_lines[index] = (
+               ntk_calls_lines[index].rstrip())
+    ntk_calls = [None] * size_of_ntk_calls_list
+    in_ntk_calls_list_index = 0
+    out_ntk_calls_list_index = 0
+    while in_ntk_calls_list_index < size_of_ntk_calls_list:
+        line_split = (
+               ntk_calls_lines[in_ntk_calls_list_index].split("\t"))
+        if line_split[-1] == "\n":
+            line_split.pop()
+        # print(in_ntk_calls_list_index, "line_split =", line_split)
+        if len(line_split) == 8:
+            ntk_calls[out_ntk_calls_list_index] = (
+                            NtkCalls(out_ntk_calls_list_index,
+                            *line_split))
+            in_ntk_calls_list_index += 1
+            out_ntk_calls_list_index += 1
+        else:
+            print(f"Error in line from file = {file_name}",
+                  f"with index = {in_ntk_calls_list_index}",
+                  f"with value {line_split}",
+                  f"wait for 8 parameters",
+                  f"and got {len(line_split)}")
+            size_of_ntk_calls_list -= 1
+            in_ntk_calls_list_index += 1
+            ntk_calls.pop()
+    return ntk_calls
+
+
 def read_ms_calls(folder, file_name):
     ms_calls_file_name = path.join(folder, file_name)
     ms_calls_file = open(ms_calls_file_name)
@@ -82,15 +164,31 @@ def read_ms_calls(folder, file_name):
     return ms_calls
 
 
+
+# main for ntk_calls part():
+ntk_calls_folder = "D:\python\double_dno\ms_ntk_in_same_amount\station 3203"
+ntk_calls_file_name = "3203_1_day_ntk.txt"
+list_of_ntk_calls = read_ntk_calls(ntk_calls_folder, ntk_calls_file_name)
+print("ntk_calls_list:")
+for record in list_of_ntk_calls:
+    print(record)
+    
+
 # main for ms_calls part():
 ms_calls_folder = "D:\python\double_dno\ms_ntk_in_same_amount\station 3203"
 ms_calls_file_name = "3203_1_day_ms.txt"
 list_of_ms_calls = read_ms_calls(ms_calls_folder, ms_calls_file_name)
 print("ms_calls_list:")
-for record in list_of_ms_calls:
-    print(record)
+#===============================================================================
+# for record in list_of_ms_calls:
+#     print(record)
+#===============================================================================
 
 hash_calls = dict()
 for record in list_of_ms_calls:
     hash_calls.update({record.hash: record})
-print(hash_calls)
+print("len(hash_calls) =", len(hash_calls))
+for record in list_of_ntk_calls:
+    hash_calls.pop(record.hash)
+print("len(hash_calls) =", len(hash_calls))
+    
